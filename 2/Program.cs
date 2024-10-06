13844dfee5404f6dcf26d9ac387bcd5ae94d0a86
Andrew <Andrew@DESKTOP-BBA8EK2>
2024-10-06 20:57:33 +0200

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+using System;
+using System.Data;
+
+class Task1
+{
+    static void Main()
+    {
+        string input;
+        do
+        {
+            WriteLine("Enter an arithmetic expression: ");
+            input = ReadLine().Replace(" ", ""); // Убираем пробелы
+            if (IsValidExpression(input))
+            {
+                try
+                {
+                    // Использование DataTable для вычисления выражений
+                    var result = new DataTable().Compute(input, null);
+                    WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine("Error in expression: " + ex.Message);
+                }
+                break;
+            }
+            else
+            {
+                WriteLine("Input error! Only numbers and +, -, *, / signs are allowed. Try again.");
+            }
+        } while (true);
+    }
+
+    // Проверка на допустимые символы
+    static bool IsValidExpression(string expression)
+    {
+        foreach (char c in expression)
+        {
+            if (!char.IsDigit(c) && c != '+' && c != '-' && c != '*' && c != '/')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ DZ/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+using System;
+
+class Task2
+{
+    static void Main()
+    {
+        try
+        {
+            string input;
+            do
+            {
+                WriteLine("Enter a sentence or text:");
+                input = ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    WriteLine("Please, enter some text.");
+            } while (string.IsNullOrWhiteSpace(input));
+
+            FormatText(input);
+        }
+        catch (Exception ex)
+        {
+            WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    // Функция для форматирования текста
+    static void FormatText(string text)
+    {
+        string[] sentences = text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string sentence in sentences)
+        {
+            string trimmedSentence = sentence.Trim();
+            if (trimmedSentence.Length > 0)
+            {
+                Write(char.ToUpper(trimmedSentence[0]) + trimmedSentence.Substring(1) + ". ");
+            }
+        }
+        WriteLine();
+    }
+}
